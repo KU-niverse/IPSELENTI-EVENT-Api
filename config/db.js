2f26fcda21c12cf468aa0890cb34d5420849bf42
youngsupchoi <112808338+youngsupchoi@users.noreply.github.com>
2023-05-09 16:44:36 +0900

    Revert "Feature/login"

--- config/db.js
+++ config/db.js
@@ -9,12 +9,10 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
-module.exports = pool;
-
-/* exports.getUsers = async () => {
+exports.getUsers = async () => {
   const [rows] = await pool.query("SELECT * FROM users");
   return rows;
-}; */
+};
 /* 예시코드 */
 
 /* export async function getNotes() {
--- middlewares/admin.js
+++
@@ -1,8 +0,0 @@
-//admin인지 확인
-exports.isAdmin = (req, res, next) => {
-  if (req.user[0].is_admin == true) {
-    next();
-  } else {
-    res.status(403).send({ success: false, message: "관리자가 아닙니다." });
-  }
-};
