8c674b96d6ae737ab9e4fe26825351798231f65e
youngsupchoi <sup1214@naver.com>
2023-05-11 16:20:37 +0900

    'feat:로그인'

---
+++ config/db.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+module.exports = pool;
+
+/* exports.getUsers = async () => {
+  const [rows] = await pool.query("SELECT * FROM users");
+  return rows;
+}; */
+/* 예시코드 */
+
+/* export async function getNotes() {
+  const [rows] = await pool.query("SELECT * FROM notes");
+
+  return rows;
+}
+
+export async function getNote(id) {
+  const [rows] = await pool.query(`SELECT * FROM notes WHERE id = ?`, [id]);
+
+  return rows[0];
+}
+
+export async function createNote(title, contents) {
+  const [result] = await pool.query(
+    `
+  INSERT INTO notes (title, contents)
+  VALUES (?, ?)
+  `,
+    [title, contents]
+  );
+  const id = result.insertId;
+  return getNote(id);
+}
+ */
---
+++ schema.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE IPSELENTI_EVENT_Api;
+USE IPSELENTI_EVENT_Api;
+
+CREATE TABLE users (
+    user_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(15) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL UNIQUE,
+    recommender_id VARCHAR(10),
+    point INT DEFAULT 0,
+    bad INT DEFAULT 0,
+    is_admin BOOL DEFAULT FALSE,
+    FOREIGN KEY (recommender_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE comments (
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    author VARCHAR(10) NOT NULL,
+    comment_content TEXT NOT NULL,
+    comment_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    likes_count INT DEFAULT 0,
+    FOREIGN KEY (author) REFERENCES users(user_id)
+);
+
+CREATE TABLE comments_like (
+    comment_id INT,
+    liker_id VARCHAR(10),
+    PRIMARY KEY (comment_id, liker_id),
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id),
+    FOREIGN KEY (liker_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE wiki_history (
+    wiki_history_id INT PRIMARY KEY AUTO_INCREMENT,
+    editor_id VARCHAR(10) NOT NULL,
+    text_pointer text,
+    edited_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_rollback INT DEFAULT 0, 
+    content_summary VARCHAR(255), /* 추가된 column */
+    FOREIGN KEY (editor_id) REFERENCES users(user_id)
+);
+
+
+
+CREATE TABLE celebrities (
+    celebrity_id INT PRIMARY KEY AUTO_INCREMENT,
+    celebrities_name VARCHAR(15) NOT NULL,
+    celebrity_image LONGBLOB, NOT NULL, /* 추가된 column */
+    betting_amount INT DEFAULT 0
+);
+
+CREATE TABLE betting_history (
+    betting_id INT PRIMARY KEY AUTO_INCREMENT,
+    celebrity_id INT NOT NULL,
+    betting_user VARCHAR(10),
+    betting_point INT,
+    betting_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (celebrity_id) REFERENCES celebrities(celebrity_id),
+    FOREIGN KEY (betting_user) REFERENCES users(user_id)
+);
+
+CREATE TABLE point_reason(
+    reason_id INT PRIMARY KEY AUTO_INCREMENT,
+    point_reason TEXT,
+    amount INT
+);
+
+CREATE TABLE point_history(
+    point_history_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(10) NOT NULL,
+    reason_id INT,
+    point_amount INT,
+    point_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (reason_id) REFERENCES point_reason(reason_id)
+);
+
+CREATE TABLE celebrity_request(
+    request_id INT PRIMARY KEY AUTO_INCREMENT,
+    requester_id VARCHAR(10) NOT NULL,
+    celebrity_name VARCHAR(15) NOT NULL,
+    request_reason VARCHAR(50),
+    request_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (requester_id) REFERENCES users(user_id)
+);
+
