2a7da0ac54c49cca1e26042cd08d12833aec35c4
youngsupchoi <sup1214@naver.com>
2023-05-16 17:27:31 +0900

    fix: index.js myPage -> mypage로 수정

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("../../middlewares/sign_in");
 
 const auth = require("./auth");
-const mypage = require("./myPage");
+const mypage = require("./mypage");
 const admin = require("./admin");
 const router = express.Router();
 
