6c730b772738a56e734b17bef61af67eeb0594e7
starcat37 <starcat37@korea.ac.kr>
2023-05-17 17:52:52 +0900

    Fix: event url 중복 내용 수정 및 user_id req에서 얻어오는 방식 변경

--- routes/event.js
+++ routes/event.js
@@ -13,13 +13,13 @@ router.get('/celebrityrequest', isSignedIn, eventMid.requestGetByIdMid);
 // GET /event/celebrities
 router.get('/celebrities', isSignedIn, eventMid.celebsGetAllMid);
 
-// GET /event/user/:userid/artist/:artistid
-router.get('/user/:userid/artist/:artistid', isSignedIn, eventMid.BettingHistoryGetMid);
+// GET /event/artist/:artistid
+router.get('/artist/:artistid', isSignedIn, eventMid.BettingHistoryGetMid);
 
-// PUT /event/user/:userid/artist/:artistid
-router.put('/user/:userid/artist/:artistid', isSignedIn, eventMid.BettingPointPutMid);
+// PUT /event/artist/:artistid
+router.put('/artist/:artistid', isSignedIn, eventMid.BettingPointPutMid);
 
-// GET /event/user/:userid
-router.get('/user/:userid', isSignedIn, eventMid.bettingGetMid);
+// GET /event/user
+router.get('/user', isSignedIn, eventMid.bettingGetMid);
 
 module.exports = router;
