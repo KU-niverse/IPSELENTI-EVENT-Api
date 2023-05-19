fad4e4065c303be690c317ea7b53e5d33ad9098c
starcat37 <starcat37@korea.ac.kr>
2023-05-20 00:44:51 +0900

    Fix: Redis 타입 에러 해결

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const pointRoutes = require("./routes/user/point");
 dotenv.config();
 const redisClient = redis.createClient({
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-  legacyMode: true,
 });
 redisClient.on('connect', () => {
   console.info(`Redis connected`);
@@ -31,7 +30,6 @@ redisClient.on('error', (err) => {
   console.error(`Redis Client Error`, err);
 });
 redisClient.connect().then();
-const redisCli = redisClient.v4;
 
 const app = express();
 passportConfig();
