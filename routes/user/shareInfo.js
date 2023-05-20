d698a6e1abdb7bd1ba68d4ec2c1b9ff7e0757ea8
youngsupchoi <sup1214@naver.com>
2023-05-20 17:03:50 +0900

    fix: consolelog제거

--- routes/user/shareInfo.js
+++ routes/user/shareInfo.js
@@ -5,7 +5,7 @@ const { isSignedIn, isNotSignedIn } = require("../../middlewares/sign_in");
 const { shareInfo } = require("../../controllers/userController/shareInfo");
 
 const router = express.Router();
-console.log("hihi");
+
 router.get("/most", isSignedIn, shareInfo);
 
 module.exports = router;
